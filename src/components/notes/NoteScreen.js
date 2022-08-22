21ea3457f0ea605c72b4cd5f91707920d7edd7b2
Haderson Bullon <hbullon@gruposerex.com>
2022-08-22 10:53:34 -0400

    Final Structure of pages and components

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -6,6 +6,28 @@ export const NoteScreen = () => {
     <div className='notes__main-content'>
 
         <NotesAppBar />
+
+        <div className='notes__content'>
+
+          <input 
+            type="text"
+            placeholder="Some awesone title"
+            className="notes__title-input"  
+          />
+
+          <textarea 
+            placeholder='What happenend today'
+            className='notes__textarea'
+          />
+
+          <div className='notes__image'>
+            <img 
+              src="https://th.bing.com/th/id/OIP.7hf8IOtnSKYT0GKTGji3ZwHaEK?pid=ImgDet&rs=1"
+              alt="imagen"
+            />
+          </div>
+
+        </div>
     
     </div>
   )
