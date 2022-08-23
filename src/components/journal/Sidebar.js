d6d6c3fa0960e88bd703619f22b2e861fe8df8c6
Haderson Bullon <hrbullon@gmail.com>
2022-08-22 21:50:51 -0400

    Display name on sidebar

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { startLogout } from '../../actions/auth'
 import { JournalEntries } from './JournalEntries'
@@ -7,6 +7,8 @@ import { JournalEntries } from './JournalEntries'
 export const Sidebar = () => {
   
     const dispatch = useDispatch();
+    const { name: displayName } = useSelector( state => state.auth )
+
 
     const handleLogout = () => {
         dispatch( startLogout() );
@@ -17,7 +19,7 @@ export const Sidebar = () => {
             <div className='journal__sidebar-navbar'>
                 <h3 className='mt-5'>
                     <i className='far fa-moon'></i>
-                    <span> Haderson</span>
+                    <span> { displayName } </span>
                 </h3>
 
                 <button 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -42,7 +42,7 @@ export const AppRouter = () => {
   }, [dispatch, setIsLoggedIn, setChecking]);
 
   if(checking){
-    return <h1>Espere...</h1>
+    return <h1>Wait...</h1>
   }
   
   return (
